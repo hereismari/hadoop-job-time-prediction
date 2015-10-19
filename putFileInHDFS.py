d82e9e6296dc97c981a3b2cb86d41fa0819d1b81
Marianne Linhares Monteiro <marianne.monteiro@ccc.ufcg.edu.br>
2015-10-19 10:51:06 -0300

    Update putFileInHDFS.py

--- putFileInHDFS.py
+++ putFileInHDFS.py
@@ -1,12 +1,31 @@
 import subprocess
 import os
 import time
-from LinuxUtils import LinuxUtils
+import sys
 
 MIN_NUM_ARGS = 3
 
 def printUsage():
         print "python PutFileInHDFS.py <filePath> <dirName>"
+
+def runHDFSCommand(args):
+    command = ["/opt/hadoop/bin/hdfs", "dfs"]
+    command += args.split()
+	
+    print command
+    proc = subprocess.Popen(command,stdout=subprocess.PIPE)
+    proc.wait()
+    return (proc.communicate(),proc.returncode)
+
+def createHDFSDir(dirPath):
+    runHDFSCommand("-mkdir -p " + dirPath)
+
+def putFileInHDFS(filePath, destPath="", blockSize=None):
+    if blockSize != None:
+        runHDFSCommand("-Ddfs.block.size=" + blockSize + " -put " + filePath + " " + destPath)
+    else:
+        runHDFSCommand("-put " + filePath + " " + destPath)
+
 
 if (len(sys.argv) < MIN_NUM_ARGS):
     print "Wrong number of arguments: ", len(sys.argv)
@@ -16,5 +35,5 @@ if (len(sys.argv) < MIN_NUM_ARGS):
 file_path = sys.argv[1]
 dir_name = sys.argv[2]
 
-HadoopUtils.createHDFSDir(dir_name)
-HadoopUtils.putFileInHDFS(file_path, dir_name)
+createHDFSDir(dir_name)
+putFileInHDFS(file_path, dir_name)
